38afa0c016050100d25740ad6aa052d110c2e07d
Luke Thompson <lpthompson92@gmail.com>
2020-12-05 17:31:01 +1100

    Add additional filters

--- src/config.js
+++ src/config.js
@@ -4,12 +4,24 @@ import {
 } from '@/constants'
 
 export const filterConfig = {
+  stage: {
+    key: 'Stage',
+    type: FILTER_TYPE_SELECT,
+  },
   category: {
     key: 'Category',
+    type: FILTER_TYPE_SELECT,
+  },
+  status: {
+    key: 'Status',
     type: FILTER_TYPE_SELECT,
   },
   value: {
     key: 'Value',
     type: FILTER_TYPE_RANGE,
+  },
+  ownership: {
+    key: 'Ownership',
+    type: FILTER_TYPE_SELECT,
   },
 }
