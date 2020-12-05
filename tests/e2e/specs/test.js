72125c11a3807bd6b2d61cff194fab853f7297f9
Luke Thompson <1659140+lukethompson@users.noreply.github.com>
2020-12-05 18:38:45 +1100

    Add basic cypress tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,22 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('Rendering', () => {
+  it('Renders the Sidebar and MapDisplay', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.get('[data-cy="sidebar"]').should('exist')
+    cy.get('[data-cy="map-display"]').should('exist')
+  })
+})
+
+describe('Filter form', () => {
+  it('Allows the user to fill out the filter form and filters the displayed map markers', () => {
+    cy.visit('/')
+    cy.get('[data-cy="sidebar__input--stage"]')
+      .type('Other')
+    cy.get('[data-cy="sidebar__input--status"]')
+      .type('Possible')
+      .type('{enter}')
+    cy.get('[data-cy="map-display__marker"]')
+      .should('have.length', 4)
   })
 })
